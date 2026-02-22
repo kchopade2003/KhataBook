f9dc88bac941d39c37669b85e43b49e43f185f69
kchopade2003 <ketanchopade3@gmail.com>
2026-02-22 23:17:31 +0530

    User Model added

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoAtlas Connected"))
+.catch(err => console.log("DB error: ", err));
+
+app.get("/", (req, res) => {
+    res.send("Khatabook API running.")
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
